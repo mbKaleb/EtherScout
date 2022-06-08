b5513d44b18da18b928680e823ffbb96d7944bd1
Kaleb Franken <skywrithin@gmail.com>
2022-06-08 11:50:40 -0600

    css improvements

--- client/src/dynamic-pages/AddressQuery.js
+++ client/src/dynamic-pages/AddressQuery.js
@@ -2,14 +2,31 @@ import { useParams } from "react-router-dom"
 
 
 
+
 export default function AddressQuery() {
 
   const { walletAddress } = useParams();
-  console.log(walletAddress)
+
 
 
   return (
-    <div className="">{walletAddress} Test</div>
+    <div className="justify-center rounded-1 bg-gray-400 p-2 w-2/3 min-w-fit">
+      <div className="Address rounded-sm mx-12 font- bg-white drop-shadow p-2">{" Address: "+ walletAddress}</div>
+      <div className="flex m-2 mx-12">
+
+        <div className="grid-flex rounded-sm bg-white drop-shadow w-6/12 mr-2 p-4 ">
+          <div className="mb-2 font-medium">Overview</div>
+          <div className="flex border-b p-1"> <div className="grow">ETH Balance: </div>   <div >0.412 Ether</div> </div>
+          <div className="flex border-b p-1"> <div className="grow">USD Est. Value: </div>   <div >$450.26 </div> </div>
+          <div className="flex border-b p-1"> <div className="grow">Wallet Type</div>   <div >0.41235123 Ether</div> </div>
+        </div>
+
+        <div className="grid-flex rounded-sm bg-white drop-shadow w-6/12 ml-2 p-4">
+          <div className="mb-2 font-medium min-w-fit">Ethereum Naming Service (ENS)</div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
@@ -26,7 +43,9 @@ export default function AddressQuery() {
 
 
 
-
+<div className='relative flex py-1 items-center'>
+  <div className='flex-grow border-t border-gray-400'></div>
+</div>
 
 
 
