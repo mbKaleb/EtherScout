c1e3bd8527bc6acd94fad33b9efe7cb16edf3079
Kaleb Franken <skywrithin@gmail.com>
2022-06-12 18:04:46 -0600

    addded .env

--- client/src/dynamic-pages/AddressQuery.js
+++ client/src/dynamic-pages/AddressQuery.js
@@ -75,30 +75,38 @@ export default function AddressQuery({currentEthPrice}) {
   }, [ensName, resolverInstance]);
 
   return (
-    <div className="justify-center rounded-1 bg-gray-400 p-2 w-screen min-w-max ">
-      <div className="Address rounded-sm mx-12 font- bg-white drop-shadow p-2">{" Address: "+ walletAddress}</div>
-      <div className="flex m-2 mx-12">
+    <div className="justify-center rounded-1 bg-gray-200 w-10/12 p-2 min-w-fit">
+      <div className="Address rounded-sm bg-white drop-shadow p-2">{" Address: "+ walletAddress}</div>
+      <div className="flex mt-2">
+
         <div className="grid-flex rounded-sm bg-white drop-shadow w-6/12 mr-2 p-4 ">
           <div className="mb-2 font-medium">Overview</div>
           <div className="flex border-b p-1"> <div className="grow">ETH Balance: </div>   <div >{walletAddress ? ethBalance +' ETH' : "Loading..."}</div> </div>
           <div className="flex border-b p-1"> <div className="grow">USD Est. Value: </div>   <div >{ethBalance ? "$"+(Math.round((ethBalance*currentEthPrice) * 100) / 100).toFixed(2) : "Loading..."}</div></div>
           <div className="flex border-b p-1"> <div className="grow">Wallet Type</div>   <div>Unknown</div> </div>
         </div>
-        <div className="[ENS Section]  grid-flex rounded-sm bg-white drop-shadow w-6/12 ml-2 p-4">
+
+        <div className="[ENS Section]  grid-flex rounded-sm bg-white drop-shadow w-6/12 ml-2 p-4 min-w-fit">
           <div className="[TITLE] mb-2 font-medium min-w-fit">Ethereum Naming Service (ENS)</div>
-          <div className="[LEFT SECTION] flex">
-            <div className="w-6/12 m-1">
-              <div className="border-b font-medium">Controller (contract)</div>
-              <div>{resolverInstance?.address || "Invalid Request"}</div>
+          <div className="[LEFT SECTION] flex min-w-fit">
+            <div className="w-6/12 m-1 min-w-fit">
+              <div className="border-b font-medium w-min-fit">Controller (contract)</div>
+              <div className="font-normal text-sm">{resolverInstance?.address || "Invalid Request"}</div>
               <div className="border-b font-medium">Email</div>
               <div>{email || 'No Record'}</div>
             </div>
-            <div className="w-6/12 m-1">
+            <div className="m-1">
               <div className="border-b font-medium">Name</div>
               <div>{ensName || "No Record"}</div>
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="rounded-sm bg-white drop-shadow w-full mt-3 p-4 ">
+        <div className="mb-2 border-b text-medium font-semibold ">Transactions</div>
+        <div className="border-b">transaction</div>
+
       </div>
     </div>
   )
